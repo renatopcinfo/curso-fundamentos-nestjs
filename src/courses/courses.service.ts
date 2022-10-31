63e2022421b9bb3d18ad23884f007d370647a456
Renato de Oliveira Rodrigues <renato@WINAPH4cpKN15z0.corp.avanade.org>
2022-10-31 15:46:26 -0300

    feat: refactoring

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -19,7 +19,7 @@ export class CoursesService {
     });
   }
 
-  async findOne(id: number) {
+  async findOne(id: string) {
     const course = await this.courseRepository.findOne({
       where: { id },
       relations: ['tags'],
@@ -53,7 +53,7 @@ export class CoursesService {
 
     const course = await this.courseRepository.preload({
       //converte string para number '+'
-      id: +id,
+      id: id,
       ...updateCourseDto,
       tags,
     });
@@ -65,7 +65,7 @@ export class CoursesService {
     return this.courseRepository.save(course);
   }
 
-  async remove(id: number) {
+  async remove(id: string) {
     const course = await this.courseRepository.findOne({
       where: { id },
     });
--- src/courses/entities/tag.entity.ts
+++ src/courses/entities/tag.entity.ts
@@ -1,15 +1,35 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Course } from "./course.entity";
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Course } from './course.entity';
+
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity('tags')
 export class Tag {
-@PrimaryGeneratedColumn()
-id:number
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-@Column()
-name: string
+  @Column()
+  name: string;
 
-@ManyToMany(() => Course, (course: Course) => course.tags)
-courses: Course[]
+  @ManyToMany(() => Course, (course) => course.tags)
+  courses: Course[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) {
+      return;
+    }
+
+    this.id = uuidv4();
+  }
 }
-
